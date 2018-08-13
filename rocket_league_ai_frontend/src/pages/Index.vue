7a3a1d5e05fecb2c9d231e43d846873b65a4c78f
Milford <malin.milford@capgemini.com>
2018-08-13 15:17:27 +0200

    vue + quasar setup

---
+++ rocket_league_ai_frontend/src/pages/Index.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="flex flex-center">
+    <h1>Rocket league</h1>
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageIndex',
+};
+</script>
