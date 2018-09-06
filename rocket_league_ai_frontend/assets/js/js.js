1a7abbc5d1275c2353147bb422a0a92d39359924
TheBerzer <berzi.w@gmail.com>
2018-09-06 16:20:12 +0200

    Inital Git of new UI
    New UI upload

---
+++ rocket_league_ai_frontend/assets/js/js.js
@@ -0,0 +1,13 @@
+/*$("#logInBtn").click(function(){
+	$("#logInModal").modal("show")
+});
+*/
+$("#signUpBtn").click(function(){
+	$("#signUpModal").modal("show")
+});
+
+$("#logInBtn").click(function(){
+	$("#navProfile").css({'display':'block'});
+    $("#logInSignUpBtns").css({'display':'none'});
+});
+
--- rocket_league_ai_frontend/src/plugins/chart.js
+++
@@ -1,5 +0,0 @@
-import Chart from 'chart.js';
-
-export default ({ Vue }) => {
-  Vue.prototype.$Chart = Chart;
-};
